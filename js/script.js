1e254fb63f21b3ff7f51eafe2a56dfd555024c46
nicchonsanchez <nicchonsanchez.ns@gmail.com>
2025-11-14 21:55:28 -0300

    Gera os números (Falta colocar o fator de repetição de números)

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+const inputNumbers = document.querySelectorAll("input[regex-number]");
+const listResult = [];
+
+// REGEX para para os inputs de número
+inputNumbers.forEach((inp) => {
+    inp.addEventListener("input", (e)=>{
+        // Regex para manter somente números
+        const numbersRegex = /[^\d]/g;
+        e.target.value = e.target.value.replace(numbersRegex, "");
+    })
+})
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    listResult.splice(0, listResult.length);
+
+    const qntNumbers = parseInt(document.getElementById("qnt-numbers").value);
+    const minNumbers = parseInt(document.getElementById("min-numbers").value);
+    const maxNumbers = parseInt(document.getElementById("max-numbers").value);
+    const repeatNumbers = document.getElementById("checkbox-reapeat").checked;
+
+    for(let rep = 0; rep < qntNumbers; rep++){
+        let newNumber = sortearNumbero(minNumbers, maxNumbers);
+        
+        listResult.push(newNumber);
+    }
+
+    console.log(listResult, repeatNumbers);
+    
+};
+
+function sortearNumbero(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
